ff34ae8d4a4571c61155fe0ff3ceeb8ecb74ff7b
daniiii2 <122824902+daniiii2@users.noreply.github.com>
2024-11-11 11:18:15 +0100

    feat(minutes): add fiveMinutesLine functionality for all cases

--- src/app.js
+++ src/app.js
@@ -5,15 +5,16 @@ export class Main {
   }
 
   fiveMinutesLine(minutes) {
-    if (minutes === 0) return "OOOOOOOOOOO";
-
-    if (minutes === 5) return "YOOOOOOOOOO";
-
-    if (minutes === 30) return "YYRYYROOOOO";
-
-    if (minutes === 55) return "YYRYYRYYRYY";
-    
-    return "OOOOOOOOOOO";
+  const onLamps = Math.floor(minutes / 5);
+  let lamps = "";
+  for (let i = 1; i <= 11; i++) {
+    if (i <= onLamps) {
+      lamps += (i % 3 === 0) ? "R" : "Y";
+    } else {
+      lamps += "O";
+    }
   }
+  return lamps;
+}
   
 }
