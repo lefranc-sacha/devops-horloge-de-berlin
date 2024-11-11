10d6015246bfb34e23133969263277859e573e25
daniiii2 <122824902+daniiii2@users.noreply.github.com>
2024-11-11 10:37:24 +0100

    refactor code in English

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,44 +1,30 @@
 import { Main } from "../src/app.js";
 
-
-describe("Horloge de Berlin - Ligne des Minutes Simples", function () {
-
+describe("Berlin Clock - Simple Minutes Line", function () {
     let main = new Main();
     
-    it("devrait éteindre toutes les lampes si les minutes sont 0", function () {
-
+    it("should turn off all lamps when minutes are 0", function () {
         let result = main.simpleMinutesLine(0);
-
         expect(result).toEqual("OOOO");
     });
-
-    it("devrait allumer une lampe si les minutes sont 1", function () {
-
+    
+    it("should turn on one lamp when minutes are 1", function () {
         let result = main.simpleMinutesLine(1);
-
         expect(result).toEqual("YOOO");
     });
-
-    it("devrait allumer deux lampes si les minutes sont 2", function () {
-
+    
+    it("should turn on two lamps when minutes are 2", function () {
         let result = main.simpleMinutesLine(2);
-
         expect(result).toEqual("YYOO");
     });
     
-    it("devrait allumer trois lampes si les minutes sont 3", function () {
-
+    it("should turn on three lamps when minutes are 3", function () {
         let result = main.simpleMinutesLine(3);
-
         expect(result).toEqual("YYYO");
     });
-
-    it("devrait allumer quatre lampes si les minutes sont 4", function () {
-
+    
+    it("should turn on four lamps when minutes are 4", function () {
         let result = main.simpleMinutesLine(4);
-        
         expect(result).toEqual("YYYY");
     });
-
-    
 });
